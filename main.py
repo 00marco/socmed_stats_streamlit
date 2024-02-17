d298ae7465fc75b98bdd375432d5bfb5f207edfc
Marco Pulido <marcopulido00@gmail.com>
2024-02-17 19:02:29 +0800

    fixed firebase connection bug

--- main.py
+++ main.py
@@ -3,16 +3,33 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import streamlit as st
 
+import json
 import pandas as pd
 
       
 def read_collection(collection_name):
     # Authenticate to Firestore with the JSON account key.
-    db = firestore.Client.from_service_account_json("socmed-analytics-firebase-adminsdk-xqyvl-15c2e7c79d.json")
+    service_account_json = {
+    "type": "service_account",
+    "project_id": st.secrets["project_id"],
+    "private_key_id": st.secrets["private_key_id"],
+    "private_key": st.secrets["private_key"],
+    "client_email": st.secrets["client_email"],
+    "client_id": st.secrets["client_id"],
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+    "universe_domain": "googleapis.com"
+    }
 
-    # Create a reference to the Google post.
-    collection_ref = db.collection(collection_name)
+    cred = credentials.Certificate(service_account_json)
+    if not firebase_admin._apps:
+        firebase_admin.initialize_app(cred)
+    client = firestore.client()
+    collection_ref = client.collection(collection_name)
     
+
     # Retrieve documents from the collection
     docs = collection_ref.stream()
     
