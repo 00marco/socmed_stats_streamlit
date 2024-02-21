893e46d9045954b0727578b95c60a1ede5de14a2
Marco Pulido <marcopulido00@gmail.com>
2024-02-21 14:54:55 +0800

    added email list collector form

--- main.py
+++ main.py
@@ -3,50 +3,86 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import streamlit as st
 import altair as alt
+import re
 
 import json
 import pandas as pd
 
-      
-def read_collection(collection_name):
-    # Authenticate to Firestore with the JSON account key.
-    service_account_json = {
-    "type": "service_account",
-    "project_id": st.secrets["project_id"],
-    "private_key_id": st.secrets["private_key_id"],
-    "private_key": st.secrets["private_key"],
-    "client_email": st.secrets["client_email"],
-    "client_id": st.secrets["client_id"],
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": st.secrets["client_x509_cert_url"],
-    "universe_domain": "googleapis.com"
-    }
+class AppUtils:
+    def __init__(self):
+        service_account_json = {
+            "type": "service_account",
+            "project_id": st.secrets["project_id"],
+            "private_key_id": st.secrets["private_key_id"],
+            "private_key": st.secrets["private_key"],
+            "client_email": st.secrets["client_email"],
+            "client_id": st.secrets["client_id"],
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+            "universe_domain": "googleapis.com"
+        }
 
-    cred = credentials.Certificate(service_account_json)
-    if not firebase_admin._apps:
-        firebase_admin.initialize_app(cred)
-    client = firestore.client()
-    collection_ref = client.collection(collection_name)
-    
+        cred = credentials.Certificate(service_account_json)
+        if not firebase_admin._apps:
+            firebase_admin.initialize_app(cred)
+        
+    def upload_record(self, collection_name, data):
+        client = firestore.client()
+        doc_ref = client.collection(collection_name).document()
+        doc_ref.set(data)
 
-    # Retrieve documents from the collection
-    docs = collection_ref.stream()
-    
-    # Iterate over the documents and print them
-    data = []
-    for doc in docs:
-        print(f"Document ID: {doc.id}")
-        print(f"Data: {doc.to_dict()['dataset_id']}, {doc.to_dict()['finished_at']}, {doc.to_dict()['engagement_score_diff']}")
-        data.append(doc.to_dict())
 
-    return data
+    def read_collection(self, collection_name):
+        # Authenticate to Firestore with the JSON account key.
+        
+        client = firestore.client()
+        collection_ref = client.collection(collection_name)
+        
+
+        # Retrieve documents from the collection
+        docs = collection_ref.stream()
+        
+        # Iterate over the documents and print them
+        data = []
+        for doc in docs:
+            print(f"Document ID: {doc.id}")
+            print(f"Data: {doc.to_dict()['dataset_id']}, {doc.to_dict()['finished_at']}, {doc.to_dict()['engagement_score_diff']}")
+            data.append(doc.to_dict())
+
+        return data
+
+    def is_valid_email(self, email):
+        """
+        Check if the provided string is a valid email address.
+        """
+        # Regular expression pattern for validating email addresses
+        email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        
+        # Match the pattern with the email string
+        match = re.match(email_pattern, email)
+        
+        # Return True if the email matches the pattern, False otherwise
+        return bool(match)
+
+appUtils = AppUtils()
+
+# Popup email list
+with st.form("my_form"):
+    st.write("Email signup")
+    email = st.text_input("Enter email here")
+    submitted = st.form_submit_button("Submit")
+    if submitted and appUtils.is_valid_email(email):
+        st.write("email", email)
+        appUtils.upload_record("email_list", {"email": email, "source": "socmed_analytics_app"})
+
+
 
 # Chart
-st.header('Tiktok stats for tiktok.com/mvrco_poloo')
+st.header('Stats for tiktok.com/mvrco_poloo')
 st.write("Engagement score")
-data = read_collection("tiktok_scraper")
+data = appUtils.read_collection("tiktok_scraper")
 df = pd.DataFrame(data).sort_values("finished_at").head(20)
 c = (
    alt.Chart(df)
